c614dc735503810966f0a5674c427629f0075081
marriet <benoitmarriet@gmail.com>
2024-02-15 22:39:25 +0100

    hope this works

---
+++ footer.php
@@ -0,0 +1,19 @@
+</main>
+
+
+<section class="pre-footer">
+    <div class="pre-footer_icons">
+        <i class="fa-brands fa-facebook-f"></i>
+        <i class="fa-brands fa-twitter"></i>
+        <i class="fa-brands fa-instagram"></i>
+    </div>
+ 
+</section>
+
+<footer>
+
+</footer>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include("header.php");
+?>
+<section>
+<?php 
+echo "my brain hurts";
+?>
+</section>
+
+<?php
+include("footer.php");
+?>
